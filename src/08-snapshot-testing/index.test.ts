5bda3e7e55861016a61dd7407423f3841f9356ad
KaterinaMysl <myslivchik382@gmail.com>
2024-02-13 00:22:44 +0300

    feat: add details

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,22 +1,31 @@
 import { generateLinkedList } from './index';
 
-describe('generateLinkedList', () => {
-  test('should generate linked list from values 1', () => {
-    const linkedList = generateLinkedList([1, 2]);
-    expect(linkedList).toStrictEqual({
-      value: 1,
+const array = [3, 4, 5, 6];
+const expected = {
+  value: 3,
+  next: {
+    value: 4,
+    next: {
+      value: 5,
       next: {
-        value: 2,
+        value: 6,
         next: {
           value: null,
           next: null,
         },
       },
-    });
+    },
+  },
+};
+
+describe('generateLinkedList', () => {
+  test('should generate linked list from values 1', () => {
+    const linkedList = generateLinkedList(array);
+    expect(linkedList).toStrictEqual(expected);
   });
 
   test('should generate linked list from values 2', () => {
-    const linkedList = generateLinkedList([2, 3, 4, 5]);
+    const linkedList = generateLinkedList(array);
     expect(linkedList).toMatchSnapshot();
   });
 });
